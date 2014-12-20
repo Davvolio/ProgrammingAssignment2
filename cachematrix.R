c01af842d9865da0de37c4e6050b163a700eb3f7
Davvolio <davvolio@gmail.com>
2014-12-21 02:29:56 +0400

    added comments
    final version

--- cachematrix.R
+++ cachematrix.R
@@ -1,47 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function create a list of functions that allow manipulations with matrix "x"
+## You can set and get the value of matrix, 
+## Also you can get and set inverse value of matix
+## When you initialise it, only set is called, and you need to pass a ready matrix
+## something like "zz<-makeCacheMatrix (matrix(rnorm(9),3,3))"
+## But beware of using matrixes like "matrix(1:9,3,3)", because you will get an error:
+## "system is exactly singular: U[3,3] = 0" and no inversion will be possible
 
 makeCacheMatrix <- function(x = matrix()) {
 
-  local <- NULL
-  set <- function(y) {
+  local <- NULL # local is omnipresent variable that will transport the values between the functions, according to the function called
+  set <- function(y) { # here we set the matrix "x"
     x <<- y
     local <<- NULL
   }
-  get <- function() x
+  get <- function() x # here we get the meaning of matrix "x" when time comes to inversion
   
-  setinversematrix <- function(solve) local <<- solve
-  getinversematrix <- function() local
+  setinversematrix <- function(solve) local <<- solve #after inversion put new matrix here
+  getinversematrix <- function() local #calling the inverted matrix from cache if original matrix was already inverted before
   
-  list(set = set, get = get,
+  list(set = set, get = get, # creating a list of 4 functions
        setinversematrix = setinversematrix,
        getinversematrix = getinversematrix)
 }
 
-zz<-makeCacheMatrix (matrix(rnorm(9),3,3))
 
-## Write a short comment describing this function
+## This function first check if new matrix was already inverted. 
+## If yes, directly put the inverted matrix from cache
+## If no, calculate the inverted matrix and put it to the cache
+## I've added some messages so you can see the workflow of code and 
+## that both matrixes (original and inverted) was visible during execution (if you will execute it out of curiousity:))
+## As a starting parameter it need the result of execution of previous makeCacheMatrix function
+## For example if "zz<-makeCacheMatrix (matrix(rnorm(9),3,3))" then "cacheSolve(zz)"
 
 cacheSolve <- function(x, ...) {
-  local <- x$getinversematrix()
-  if(!is.null(local)) {
-    message("we have the cache of this matrix:")
-    rawmatrix <- x$get()
+  local <- x$getinversematrix() # first step, check the inverted matrix
+  if(!is.null(local)) { # if there is cached matrix came here
+    message("we have the cache of this matrix:") #message to show original matrix
+    rawmatrix <- x$get() # getting original matrix from makeCacheMatrix function
     print (rawmatrix)
     message ('and here it is!')
-    return(local)
+    return(local) # returning the inverted matrix from cache
   }
-  rawmatrix <- x$get()
-  local <- solve(rawmatrix, ...)
-  x$setinversematrix(local)
+  rawmatrix <- x$get() #came here if no cache found, for inversion take original matrix from makeCacheMatrix function
+  local <- solve(rawmatrix, ...) # inverse matrix using the recommened "solve" function
+  x$setinversematrix(local) # putting to cache inverted matrix in makeCacheMatrix function
   message ('no cache was found for matrix...')
   print (rawmatrix)
   message ('so we calculated it here!')
-  local
-  ## Return a matrix that is the inverse of 'x'
+  local # Return a matrix that is the inverse of 'x'
+
 }
-
-cacheSolve(zz)
-
