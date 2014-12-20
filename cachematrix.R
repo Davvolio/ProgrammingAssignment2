e345b63257bda0d39b78e9c525dc5e733f92937c
Davvolio <davvolio@gmail.com>
2014-12-21 01:46:22 +0400

    functions
    first tries

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,33 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinversematrix <- function(solve) m <<- solve
+  getmean <- function() m
+  list(set = set, get = get,
+       setinversematrix = setinversematrix,
+       getmean = getmean)
 }
 
-
+zz<-makeCacheMatrix (matrix(rnorm(9),3,3))
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinversematrix(m)
+  m
+  ## Return a matrix that is the inverse of 'x'
 }
+
+cacheSolve(zz)
